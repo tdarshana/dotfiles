21a7b1c60e6f8c53b29d8d832c7bbd9a95199594
DarshanaWT <tharindu24darshana@gmail.com>
2024-11-17 11:21:50 +0800

    Local Changes - Wezterm Mods, LazyGit to NVim
    Added LazyGit Plugin to Nvim
    Modified Wezterm Looks
    TMux Modifications

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,14 +10,14 @@ return {
 	window_padding = {
 		left = "2cell",
 		right = "2cell",
-		top = "0.5cell",
-		bottom = "0.5cell",
+		top = "0.2cell",
+		bottom = "0.2cell",
 	},
 	window_frame = {
 		border_left_width = "1px",
 		border_right_width = "1px",
 		border_bottom_height = "1px",
-		border_top_height = "1px",
+		border_top_height = "10px",
 		border_left_color = "#191B29",
 		border_right_color = "#191B29",
 		border_bottom_color = "#191B29",
@@ -35,29 +35,29 @@ return {
 	default_prog = { "/opt/homebrew/bin/fish", "-l" },
 
 	-- Enable or disable the tab bar
-	enable_tab_bar = false,
+	enable_tab_bar = true,
 	tab_bar_at_bottom = false,
 	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
+	hide_tab_bar_if_only_one_tab = false,
 	tab_max_width = 20,
 
 	-- Set window decorations
-	-- window_decorations = "INTEGRATED_BUTTONS|RESIZE",
-	window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW",
+	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+	-- window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW",
 
 	-- Theme
 	color_scheme = "Tokyo Night",
-	window_background_opacity = 0.95,
-	macos_window_background_blur = 30,
-	--colors = {
-	--	background = "#051529",
-	--},
+	window_background_opacity = 0.80,
+	macos_window_background_blur = 35,
+	colors = {
+		background = "#061d38",
+	},
 
 	-- Keybindings
 	--
 	-- Mouse Bindings
 	-- Disabled Mouse
-	disable_default_mouse_bindings = true,
+	-- disable_default_mouse_bindings = true,
 	pane_focus_follows_mouse = false,
 	hide_mouse_cursor_when_typing = true,
 	mouse_wheel_scrolls_tabs = true,
